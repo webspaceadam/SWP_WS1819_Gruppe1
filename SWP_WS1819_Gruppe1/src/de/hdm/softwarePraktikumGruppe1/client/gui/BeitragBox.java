f48bb3ecb3c77b04862a01f162ced4e546bd463f
Adam <neunfuenf@gmail.com>
2018-11-26 21:51:01 +0100

    Added first Logic for Like counting in BeitragBox

--- SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/client/gui/BeitragBox.java
+++ SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/client/gui/BeitragBox.java
@@ -2,6 +2,9 @@ package de.hdm.softwarePraktikumGruppe1.client.gui;
 
 import java.util.Date;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.*;
 
@@ -20,12 +23,15 @@ public class BeitragBox extends FlowPanel {
 	private FlowPanel creationInfoWrapper = new FlowPanel();
 	private FlowPanel contentWrapper = new FlowPanel();
 	private FlowPanel socialWrapper = new FlowPanel();
+	private HorizontalPanel likeInfoWrapper = new HorizontalPanel();
 	private HTML hrElement = new HTML("<hr/>");
 	
 	// Labels
 	private Label accountName = new Label("Sebastian Hermann");
 	private Label nickName = new Label("@sebmeister");
 	private Label creationDate = new Label();
+	private Label likeCountText = new Label();
+	private int likeCount = 0;
 	
 	// Buttons for Social
 	private Button commentBtn = new Button("Kommentiere");
@@ -52,6 +58,7 @@ public class BeitragBox extends FlowPanel {
 		creationDate.addStyleName("is-size-7");
 		userInfoWrapper.addStyleName("grid_box content_margin");
 		creationInfoWrapper.addStyleName("content_margin");
+		contentWrapper.addStyleName("content_margin");
 		
 		// Social Wrapper
 		socialWrapper.addStyleName("grid_box_links");
@@ -65,6 +72,16 @@ public class BeitragBox extends FlowPanel {
 		
 		creationDate.setText("Erstellungszeitpunkt: " + date);
 		
+		likeCountText.addStyleName("is-size-6 is-italic");
+		likeCountText.setText("Likes auf diesem Beitrag: " + likeCount);
+		
+		
+		likeInfoWrapper.add(likeCountText);
+		
+		// Add ClickHandler for Button
+		likeBtn.addClickHandler(new LikeCountClickHandler(this));
+		
+		
 		// Add Elements to Wrapper
 		userInfoWrapper.add(accountName);
 		userInfoWrapper.add(nickName);
@@ -76,11 +93,35 @@ public class BeitragBox extends FlowPanel {
 		this.add(userInfoWrapper);
 		this.add(creationInfoWrapper);
 		this.add(contentWrapper);
+		this.add(likeInfoWrapper);
 		this.add(hrElement);
 		this.add(socialWrapper);
 	}
 	
 	public void onLoad() {
 		
+	}
+	
+	/**
+	 * Die innere Klasse <code>LikeCountClickHandler</code> implementiert das Clickhandler 
+	 * Interface und dessen dazugehörige <code>onClick(ClickEvent event)</code> Methode.
+	 * Diese Methode arbeitet mit der Anzahl der Likes auf einem Beitrag und passt die Anzahl auf den 
+	 * dazugehörigen Beitrag an. 
+	 * @author Adam Gniady
+	 *
+	 */
+	private class LikeCountClickHandler implements ClickHandler {
+		private BeitragBox parentBB;
+		
+		public LikeCountClickHandler(BeitragBox bb) {
+			parentBB = bb;
+		}
+			
+		@Override
+		public void onClick(ClickEvent event) {
+			parentBB.likeCount += 1;
+			parentBB.likeCountText.setText("Likes auf diesem Beitrag: " + parentBB.likeCount);
+			GWT.log("Like Count is: " + parentBB.likeCount);
+		}
 	}
 }
