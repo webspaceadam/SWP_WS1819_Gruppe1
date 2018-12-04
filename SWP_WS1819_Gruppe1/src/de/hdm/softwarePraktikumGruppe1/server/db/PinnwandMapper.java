3a99fbe7ace9c8319bfb93ad9fdbcdb46cca5546
serhat <ulus@LAPTOP-F2Q16QJC>
2018-12-04 13:09:55 +0100

    PinnwandMapper so gut wie fertig

--- SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/server/db/PinnwandMapper.java
+++ SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/server/db/PinnwandMapper.java
@@ -1,55 +1,113 @@
-/**
- * 
- */
+
 package de.hdm.softwarePraktikumGruppe1.server.db;
+
+import java.sql.*;
 
 import de.hdm.softwarePraktikumGruppe1.shared.bo.Beitrag;
 import de.hdm.softwarePraktikumGruppe1.shared.bo.Like;
 import de.hdm.softwarePraktikumGruppe1.shared.bo.Pinnwand;
+import de.hdm.softwarePraktikumGruppe1.shared.bo.User;
 
 /**
- * @author GianlucaBernert
- * @author Yesin Soufi
+ * @author GianlucaBernert, SerhatUlus
  *
  */
-public class PinnwandMapper {
-	
-	private static PinnwandMapper pinnwandMapper;
-	/**
-	 * Ein geschï¿½tzter Konstruktor der weitere Instanzierungen von PinnwandMapper Objekten verhindert.
-	 */
-	protected PinnwandMapper() {
-	}
-	
-	/**
-	 * Stellt die Singeleton-Eigenschaft der Mapperklasse sicher
-	 * @return Sie gibt den PinnwandMapper zurï¿½ck.
-	 */
-	
-	public static PinnwandMapper pinnwandMapper() {
-		if (pinnwandMapper == null) {
-			pinnwandMapper = new PinnwandMapper();
-		} 
+	public class PinnwandMapper {
 		
-		return pinnwandMapper;
-	}
+		// Variable die besagt ob schon nutzerMapperverbindung besteht
+		private static PinnwandMapper pinnwandMapper = null;
 
+		
+		//leerer Konstruktor wegen Singleton
+		protected PinnwandMapper() {
+		}
+
+		
+		//Singleton "Konstruktor"-methode
+		 public static PinnwandMapper pinnwandMapper(){
+			if (pinnwandMapper==null){
+				 pinnwandMapper= new PinnwandMapper();
+			}
+			 return pinnwandMapper;
+			}
+		 
+		 
+		 
+		 public void insertPinnwand (Pinnwand p) {
+			 
+			 Connection con = DBConnection.connection();
+		 
+		 try{
+			 
+			 Statement stmt = con.createStatement();
+			 
+			        // Jetzt erst erfolgt die tatsächliche Einfügeoperation
+			        stmt.executeUpdate("INSERT INTO pinnwand (PinnwandID, UserID, CreationTimeStamp) "
+			            + "VALUES (" + p.getId(p) + ",'" + p.getOwnerId()+ ",'" + p.getCreationTimeStamp());
+		      	
+		    }
+			
+		    catch (SQLException e) {
+		      e.printStackTrace();
+		    }
+			 
+
+		 }
+
+
+
+		public void deletePinnwand(Pinnwand p) {
+			
+
+			 Connection con = DBConnection.connection();
+			 
+		 try {
+			 Statement stmt = con.createStatement();
+			 
+			 
+			 stmt.executeUpdate("DELETE FROM `pinnwand` " + "WHERE `UserId`=" + p.getOwnerId() + " `pinnwandID`=" +p.getId(p));
+		 }
+		 
+		 catch (SQLException e) {
+		      e.printStackTrace();
+		    }
+			 
+
+		}
+		 
 	
-	
-	public void insertPinnwand(Pinnwand p) {
+		 public Pinnwand findPinnwandByUserID(User u) {
+				
+				//Aufbau der DBVerbindung
+				Connection con = DBConnection.connection();
+						
+				//Versuch der Abfrage
+				try{
+					Statement stmt = con.createStatement();
+					//Suche alle Felder der Pinnwandtabelle anhand von ID
+					ResultSet rs = stmt.executeQuery("SELECT * FROM pinnwand " + "WHERE UserID=" + u.getId(u));
+
+					 //Maximal ein Rückgabewert da Id Primärschlüssel
+					if (rs.next()) {
+						// Ergebnis in Pinnwandobjekt umwandeln
+						Pinnwand p = new Pinnwand();
+						p.setId(rs.getInt("PinnwandID"));
+						p.setCreationTimeStamp(rs.getDate("CreationTimeStamp"));
+						p.setOwner(u);   	
+						return p;
+						}
+					}
+						
+					catch (SQLException e) {
+					   e.printStackTrace();
+					   
+					  }
+				return null;
+		 }
 	}
-	
-	/**
-	 * Methode zum loeschen einer Pinnwand
-	 */
-	public void deletePinnwand(Pinnwand p) {
-	}
-	
-	/**
-	 * Methode zum zaehlen aller Likes eines Beitrags
-	 */
-	public int countAllLikesFromBeitrag(Beitrag b, Like l) {
-	return 0;
-	}
-	
-}
+			 
+			 
+			 
+			 
+			 
+			 
