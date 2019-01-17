0510fd46f6941391096b15a24dfd7e8880a7bd39
JakobBenkoe <jakob.benkoe@gmail.com>
2019-01-17 16:56:30 +0100

    changed server side implementation of loginService

--- SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/server/LoginServiceImpl.java
+++ SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/server/LoginServiceImpl.java
@@ -3,23 +3,41 @@
  */
 package de.hdm.softwarePraktikumGruppe1.server;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.softwarePraktikumGruppe1.client.LoginService;
 import de.hdm.softwarePraktikumGruppe1.shared.LoginInfo;
-import de.hdm.softwarePraktikumGruppe1.shared.LoginService;
+
 
 /**
  * @author GianlucaBernert
  * Klasse die LoginService Implementiert und RemoteServiceServlet als Superklasse besitzt
  */
-public class LoginServiceImpl extends RemoteServiceServlet implements LoginService{
+public class LoginServiceImpl extends RemoteServiceServlet implements LoginService {
 
-	/**
+/**
 	 * 
 	 */
-	public LoginInfo login(String requestURL) {
-		return null;
-	}
-	
+	private static final long serialVersionUID = 1L;
+
+public LoginInfo login(String requestUri) {
+UserService userService = UserServiceFactory.getUserService();
+User user = userService.getCurrentUser();
+LoginInfo loginInfo = new LoginInfo();
+
+if (user != null) {
+  loginInfo.setLoggedIn(true);
+  loginInfo.setEmailAddress(user.getEmail());
+  loginInfo.setNickname(user.getNickname());
+  loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+} else {
+  loginInfo.setLoggedIn(false);
+  loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+}
+return loginInfo;
+}
 
 }
