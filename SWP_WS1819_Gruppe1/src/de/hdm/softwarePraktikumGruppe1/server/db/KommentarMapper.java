af9baeee2f6ad56e62847a01ebdb60487cce4478
YesinSoufi <ys018@hdm-stuttgart.de>
2019-02-03 13:36:52 +0100

    KommentarMapper angepasst

--- SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/server/db/KommentarMapper.java
+++ SWP_WS1819_Gruppe1/src/de/hdm/softwarePraktikumGruppe1/server/db/KommentarMapper.java
@@ -50,8 +50,10 @@ public class KommentarMapper {
 	  * in allen Mapper-Klassen existieren.
 	  */
 	 
-	 /*
+	 /**
 	  * Methode, die einen Kommentar ahnand einer Id zurueckgibt
+	  * @param kommentarId
+	  * @return k
 	  */
 	 	public Kommentar findKommentarById(int kommentarId){
 			
@@ -89,8 +91,9 @@ public class KommentarMapper {
 				return null;
 		 } 
 	
-	 /*
+	 /**
 	  * Methode, die das Anlegen eines Kommentarobjekts in der Datenbank ermöglicht.
+	  * @param k
 	  */
 	 	public void insertKommentar(Kommentar k){
 	
@@ -111,8 +114,9 @@ public class KommentarMapper {
 			}
 	 	}
 	 	
-	 /*
+	 /**
 	  * Methode, die das Updaten eines Kommentarobjekts in der Datenbank ermöglicht.
+	  * @param k
 	  */
 	 	 public void updateKommentar(Kommentar k){
 			//Aufbau der DBVerbindung
@@ -130,7 +134,7 @@ public class KommentarMapper {
 				    } 
 			 }
 	 	
-	 /*
+	 /**
 	  * Methode, die das Löschen eines Beitragobjekts aus der Datenbank ermöglicht.
 	  */		 	
 	 	public void deleteKommentar(Kommentar k){
@@ -151,8 +155,10 @@ public class KommentarMapper {
 	 * Beginn: Foreign Key-Mapper-Methoden
 	 */	    
 		 
-	 	/*
+	 	/**
 		 * Methode, die einen Vector mit allen Kommentaren eines Users zurückgibt.
+		 * @param userId
+		 * @return result
 		 */
 		 public Vector<Kommentar> findKommentareOfUser(int userId){
 			//Aufbau der DBVerbindung
@@ -184,8 +190,10 @@ public class KommentarMapper {
 	 
 		 }
 		 
-		 /*
+		 /**
 		  * Methode, die einen Vector mit allen Kommentaren eines Beitrags zurückgibt.
+		  * @param beitragId
+		  * @return result
 		 */ 
 		 public Vector<Kommentar> findKommentareOfBeitrag(int beitragId){
 			//Aufbau der DBVerbindung
@@ -218,8 +226,12 @@ public class KommentarMapper {
 		 }
 
 		 
-		 /*
+		 /**
 		  * Methode, die einen Vector mit Kommentaren in einem Zeitraum eines Beitrags zurückgibt.
+		  * @param beitragId
+		  * @param start
+		  * @param end
+		  * @return result
 		 */ 
 		 public Vector<Kommentar> findKommentareOfBeitrag(int beitragId, Date start, Date end){
 			//Aufbau der DBVerbindung
